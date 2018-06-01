663b5d8d06592f5cf486932f60c5ad675fe2d5f4
Kevin Davis <kldavis.244@gmail.com>
2018-06-01 13:54:40 -0500

    starting mobile orientation

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,6 +4,7 @@
 <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/apple-touch-icon.png') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16x16.png') }}">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src='https://www.google.com/recaptcha/api.js'></script>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,55 @@
 <header class="one-whole" id="navbar">
 
     <div class="site-header one-whole">
-        <div class="one-third float-left">
-            <i class="fa fa-code navbar-code" aria-hidden="true"></i>
+
+        <div class="desktop-header">
+            <div class="one-third float-left">
+                <i class="fa fa-code navbar-code" aria-hidden="true"></i>
+            </div>
+            <div class="one-third float-left">
+                <a href="/" id="nav-home" class="navbar-items">Home</a>
+                <a href="/about" id="nav-about" class="navbar-items">About Me</a>
+                <a href="/portfolio" id="nav-port" class="navbar-items">Portfolio</a>
+                <a href="/contact" id="nav-contact" class="navbar-items">Contact Me</a>
+            </div>
+            <div class="one-third float-left">
+                <i class="navbar-bracket float-right" aria-hidden="true">{ }</i>
+            </div>
         </div>
-        <div class="one-third float-left">
-            <a href="/" id="nav-home" class="navbar-items">Home</a>
-            <a href="/about" id="nav-about" class="navbar-items">About Me</a>
-            <a href="/portfolio" id="nav-port" class="navbar-items">Portfolio</a>
-            <a href="/contact" id="nav-contact" class="navbar-items">Contact Me</a>
+
+        <div class="mobile-header">
+            <div class="one-third float-left">
+                <i class="fa fa-code navbar-code" aria-hidden="true"></i>
+            </div>
+            <div class="one-third float-left mobile-menu-container">
+                <div class="w3-container">
+                    <div class="w3-dropdown-click">
+                        <i onclick="myFunction()" class="fa fa-bars" aria-hidden="true"> <span class="mobile-menu">Menu</span></i>
+                        <div id="Demo" class="w3-dropdown-content w3-bar-block w3-border">
+                            <a href="/" class="w3-bar-item w3-button">Home</a>
+                            <a href="/about" class="w3-bar-item w3-button">About Me</a>
+                            <a href="/portfolio" class="w3-bar-item w3-button">Portfolio</a>
+                            <a href="/contact" class="w3-bar-item w3-button">Contact Me</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="one-third float-left">
+                <i class="navbar-bracket float-right" aria-hidden="true">{ }</i>
+            </div>
         </div>
-        <div class="one-third float-left">
-            <i class="navbar-bracket float-right" aria-hidden="true">{ }</i>
-        </div>
+
     </div>
+
+    <script>
+        function myFunction() {
+            var x = document.getElementById("Demo");
+            if (x.className.indexOf("w3-show") == -1) {
+                x.className += " w3-show";
+            } else {
+                x.className = x.className.replace(" w3-show", "");
+            }
+        }
+    </script>
 
 </header>
